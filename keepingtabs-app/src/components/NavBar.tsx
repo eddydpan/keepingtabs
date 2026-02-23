06dbf5958eb6620f36d4d23697571fde24a94e05
coxlaurel <coxlaurel@gmail.com>
2026-02-23 15:59:54 +0800

    navbar xs text

--- keepingtabs-app/src/components/NavBar.tsx
+++ keepingtabs-app/src/components/NavBar.tsx
@@ -4,7 +4,7 @@
 const NavBar = () => {
     return (
         <>
-            <div className="min-w-screen min-h-18 bg-[#FFFFFF] shadow-[0_-4px_12px_rgba(0,0,0,0.15)] flex flex-row justify-around items-center rounded-t-lg">
+            <div className="min-w-screen min-h-18 bg-[#FFFFFF] shadow-[0_-4px_12px_rgba(0,0,0,0.15)] flex flex-row justify-around items-center rounded-t-lg text-xs">
                 <p>expenses</p>
                 <p>tabs</p>
                 <p>request expense</p>
