66b8f4615cac474fd3ad62476a65db83c052ea15
coxlaurel <coxlaurel@gmail.com>
2026-02-23 13:04:56 +0800

    create react-ts-vite template

---
+++ keepingtabs-app/src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+// import React from "react";
+
+const NavBar = () => {
+    return (
+        <>
+        <p>navbar</p>
+        <div>
+            <p>expenses</p>
+            <p>tabs</p>
+            <p>request expense</p>
+            <p>friends</p>
+            <p>me</p>
+        </div>
+        </>
+        
+    )
+}
+
+export default NavBar;
