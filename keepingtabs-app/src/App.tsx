1b25e53e503889a50116ccacabfd77a95516e8c0
eddydpan <eddydotpan@gmail.com>
2026-02-24 13:13:37 +0800

    Create supabase client in App.tsx

--- keepingtabs-app/src/App.tsx
+++ keepingtabs-app/src/App.tsx
@@ -1,5 +1,12 @@
+import { useState } from 'react'
+import { createClient } from "@supabase/supabase-js"; 
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar';
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 function App() {
 
