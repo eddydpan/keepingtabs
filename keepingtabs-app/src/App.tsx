85a9c47870ba90bce3be8940538f0350dcd96220
eddydpan <eddydotpan@gmail.com>
2026-02-24 14:18:44 +0800

    implement initial supabase integration in App.tsx

--- keepingtabs-app/src/App.tsx
+++ keepingtabs-app/src/App.tsx
@@ -1,17 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { createClient } from "@supabase/supabase-js"; 
+import type { Database } from "./types/database.types";
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar';
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
-function App() {
+type User = Database["public"]["Tables"]["users"]["Row"];
 
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+    useEffect(() => {
+      getUsers();
+    }, []);
+
+  const getUsers = async () => {
+    // TODO: This query fetches all users — scope this to relevant users once auth is set up
+    const { data, error } = await supabase.from("users").select("*");
+    console.log("data:", data);
+    console.log("error:", error);
+    if (error) {
+      console.error("Error fetching users:", error);
+    } else {
+      setUsers(data);
+    }
+  };
+
+  
   return (
     <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.user_id}>{user.user_id}</li>
+        ))}
+      </ul>
       <div className='absolute bottom-0 left-0'>
         <NavBar />
       </div>
