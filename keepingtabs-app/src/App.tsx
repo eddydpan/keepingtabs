0747e7f88b2466bf6c088cb0c6989c53a97b4db3
coxlaurel <coxlaurel@gmail.com>
2026-02-24 14:26:01 +0800

    react router basic page pathing

--- keepingtabs-app/src/App.tsx
+++ keepingtabs-app/src/App.tsx
@@ -1,13 +1,43 @@
-import './App.css'
-import NavBar from './components/NavBar';
+import './App.css';
+// import NavBar from './components/NavBar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ExpensePage from './features/expenses/ExpensePage';
+import TabsPage from './features/tabs/TabsPage';
+import RequestExpensePage from './features/request-expense/RequestExpensePage';
+import FriendsPage from './features/friends/FriendsPage';
+import MePage from './features/me/MePage';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <ExpensePage />
+    },
+    {
+      path: "/tabs",
+      element: <TabsPage />
+    },
+    {
+      path: "/request-expense",
+      element: <RequestExpensePage />
+    },
+    {
+      path: "/friends",
+      element: <FriendsPage />
+    },
+    {
+      path: "/me",
+      element: <MePage />
+    }
+  ])
+
   return (
     <>
-      <div className='absolute bottom-0 left-0'>
+      <RouterProvider router = {router} />
+      {/* <div className='absolute bottom-0 left-0'>
         <NavBar />
-      </div>
+      </div> */}
     </>
   )
 }
