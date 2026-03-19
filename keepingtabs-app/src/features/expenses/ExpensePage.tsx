263e55df5c8d03cc12454a5f3da49d28c296b8b0
coxlaurel <coxlaurel@gmail.com>
2026-03-19 14:07:23 +0800

    placeholder layout for expense card

--- keepingtabs-app/src/features/expenses/ExpensePage.tsx
+++ keepingtabs-app/src/features/expenses/ExpensePage.tsx
@@ -1,9 +1,27 @@
 // import React from 'react'
 
+
+
+const ExpenseCard = () => {
+  return (
+    <>
+      <div className="flex flex-row justify-between m-4">
+        <div className="flex flex-col">
+          <p>Username</p>
+          <p>time</p>
+          <p>note</p>
+        </div>
+        <p>amount</p>
+      </div>
+    </>
+  )
+}
+
 const ExpensePage = () => {
   return (
     <>
       <div>ExpensePage</div>
+      <ExpenseCard />
     </>
 
   )
