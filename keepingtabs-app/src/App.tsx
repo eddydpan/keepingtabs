810787b52ae8fd46e233816bd8f7f2c6c24e8ac6
eddydpan <eddydotpan@gmail.com>
2026-04-23 14:35:27 +0800

    Remove duplicated router from merge

--- keepingtabs-app/src/App.tsx
+++ keepingtabs-app/src/App.tsx
@@ -11,33 +11,6 @@ import FriendsPage from './features/friends/FriendsPage';
 import MePage from './features/me/MePage';
 import LoginPage from './features/auth/LoginPage';
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <LoginPage />
-  },
-  {
-    path: "/",
-    element: <ProtectedRoute><ExpensePage /></ProtectedRoute>
-  },
-  {
-    path: "/tabs",
-    element: <ProtectedRoute><TabsPage /></ProtectedRoute>
-  },
-  {
-    path: "/request-expense",
-    element: <ProtectedRoute><RequestExpensePage /></ProtectedRoute>
-  },
-  {
-    path: "/friends",
-    element: <ProtectedRoute><FriendsPage /></ProtectedRoute>
-  },
-  {
-    path: "/me",
-    element: <ProtectedRoute><MePage /></ProtectedRoute>
-  }
-])
-
 
 import { Outlet } from "react-router-dom";
 
